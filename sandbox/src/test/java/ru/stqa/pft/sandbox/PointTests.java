e8a7327313c99054cb8af1ce8d3a03103bef05c1
kvlazarenko <kv.lazarenko@gmail.com>
2025-02-07 07:26:11 +0300

    Лекция 2.2 Лекция 2.2. Делаем тест для веб-приложения, используя драйвер Selenium

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,13 +3,15 @@ package ru.stqa.pft.sandbox;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 public class PointTests {
 
 	public double pointdistance;
 
 	@Test
 	public void testsPoint() {
-		Point p1 = new Point(1.9, 6.0);
+		Point p1 = new Point(1, 6);
 		Point p2 = new Point(2, 4);
 		pointdistance = p1.distance(p2);
 		System.out.println(p1.distance(p2));
@@ -18,7 +20,7 @@ public class PointTests {
 
 	@Test
 	public void testsPoint2() {
-		Point p1 = new Point(1.9, 6.0);
+		Point p1 = new Point(1, 6);
 		Point p2 = new Point(2, 4);
 		pointdistance = p2.distance(p1);
 		System.out.println(p2.distance(p1));
