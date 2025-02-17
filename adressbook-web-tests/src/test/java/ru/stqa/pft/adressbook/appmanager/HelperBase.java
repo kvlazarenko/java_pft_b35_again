ff8ded6b5d4e32823aa4f0d242f90b37e7ac6a1f
kvlazarenko <kv.lazarenko@gmail.com>
2025-02-17 19:55:40 +0300

    Лекция 3.5. Оптимизация заполнения форм оставляем дефолтные значения в полях.mp4

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/HelperBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/HelperBase.java
@@ -6,17 +6,23 @@ import org.openqa.selenium.WebDriver;
 
 public class HelperBase {
 	protected WebDriver wd;
+
 	public HelperBase(WebDriver wd) {
 		this.wd = wd;
 	}
+
 	protected void click(By locator) {
 		wd.findElement(locator).click();
 	}
+
 	protected void type(By locator, String text) {
 		click(locator);
-		wd.findElement(locator).clear();
-		wd.findElement(locator).sendKeys(text);
+		if (text != null) {
+			wd.findElement(locator).clear();
+			wd.findElement(locator).sendKeys(text);
+		}
 	}
+
 	public boolean isAlertPresent() {
 		try {
 			wd.switchTo().alert();
@@ -26,3 +32,4 @@ public class HelperBase {
 		}
 	}
 }
+
