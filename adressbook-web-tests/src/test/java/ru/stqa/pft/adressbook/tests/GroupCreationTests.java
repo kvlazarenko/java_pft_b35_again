f773c4a5da91dc8df01dfbb2b554c4eeb27b22a2
kvlazarenko <kv.lazarenko@gmail.com>
2025-02-21 07:33:05 +0300

    Лекция 4.3. Количество элементов в коллекции.mp4

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.adressbook.model.GroupDate;
 
@@ -10,6 +11,9 @@ public class GroupCreationTests extends TestBase {
 	@Test
 	public void testGroupCreation() throws Exception {
 		app.getNavigationHelper().gotoGroupPage();
+		int before = app.getGroupHelper().getGroupCount();
 		app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
+		int after = app.getGroupHelper().getGroupCount();
+		Assert.assertEquals(after, before + 1);
 	}
 }
