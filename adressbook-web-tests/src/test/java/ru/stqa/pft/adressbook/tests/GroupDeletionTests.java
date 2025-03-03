bf7b33c8ce521ddb96b56c41d3f2e3bc11a9604e
kvlazarenko <kv.lazarenko@gmail.com>
2025-03-03 08:36:56 +0300

    Лекция 5.2. Улучшение кода тестов. ещё немного реструктуризации кода.mp4

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.adressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupDate;
 
@@ -9,24 +10,25 @@ import java.util.HashSet;
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
-
-	@Test
-	public void testGroupDeletion() throws Exception {
-
+	@BeforeMethod
+	public void ensurePreconditions() {
 		app.getNavigationHelper().gotoGroupPage();
 		if (!app.getGroupHelper().isThereAGroup()) {
 			app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
 		}
+	}
+
+	@Test
+	public void testGroupDeletion() throws Exception {
 		List<GroupDate> before = app.getGroupHelper().getGroupList();
-		app.getGroupHelper().selectGroup(before.size() - 1);
+		int index = before.size() - 1;
+		app.getGroupHelper().selectGroup(index);
 		app.getGroupHelper().deleteSelectedGroups();
 		app.getGroupHelper().returnToGroupPage();
 		List<GroupDate> after = app.getGroupHelper().getGroupList();
-		Assert.assertEquals(after.size(), before.size() - 1);
-
-		before.remove(before.size() - 1);
+		Assert.assertEquals(after.size(), index);
+		before.remove(index);
 		Assert.assertEquals(before, after);
-
 		Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 	}
 }
--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.adressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupDate;
 
@@ -9,29 +10,28 @@ import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
-
-	@Test
-	public void testGroupModification() {
+	@BeforeMethod
+	public void ensurePreconditions() {
 		app.getNavigationHelper().gotoGroupPage();
 		if (!app.getGroupHelper().isThereAGroup()) {
 			app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
 		}
+	}
+
+	@Test
+	public void testGroupModification() {
 		List<GroupDate> before = app.getGroupHelper().getGroupList();
-		app.getGroupHelper().selectGroup(before.size() - 1);
-		app.getGroupHelper().initGroupModification();
-		GroupDate group = new GroupDate(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
-		app.getGroupHelper().fillGroupForm(group);
-		app.getGroupHelper().submitGroupModification();
-		app.getGroupHelper().returnToGroupPage();
+		int index = before.size() - 1;
+		GroupDate group = new GroupDate(before.get(index).getId(), "test1", "test2", "test3");
+		app.getGroupHelper().modifyGroup(index, group);
 		List<GroupDate> after = app.getGroupHelper().getGroupList();
 		Assert.assertEquals(after.size(), before.size());
-
-
-		before.remove(before.size() - 1);
+		before.remove(index);
 		before.add(group);
-
 		before.sort(Comparator.comparing(GroupDate::getId));
 		after.sort(Comparator.comparing(GroupDate::getId));
 		Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 	}
+
+
 }
