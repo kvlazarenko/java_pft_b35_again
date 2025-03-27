50189d0c7d13b22c200c84efa492b47c556bd8fa
kvlazarenko <kv.lazarenko@gmail.com>
2025-03-27 20:46:05 +0300

    Лекция 6.6. Формат XML.mp4

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupDate;
@@ -9,6 +10,8 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,15 +19,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 	@DataProvider
 	public Iterator<Object[]> validGroups() throws IOException {
-		List<Object[]> list = new ArrayList<Object[]>();
-		BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+		BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+		String xml = "";
 		String line = reader.readLine();
 		while (line != null) {
-			String[] split = line.split(";");
-			list.add(new Object[]{new GroupDate().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+			xml += line;
 			line = reader.readLine();
 		}
-		return list.iterator();
+		XStream xstream = new XStream();
+		xstream.processAnnotations(GroupDate.class);
+		List<GroupDate> groups = (List<GroupDate>) xstream.fromXML(xml);
+		return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
 	}
 
 
