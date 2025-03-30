e945abe13ffe494b0dce719d5cc9d8ac2b3b320b
kvlazarenko <kv.lazarenko@gmail.com>
2025-03-30 11:46:03 +0300

    Задание №13 Параметризовать тесты для создания групп и контактов.png

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactCreationTests.java
@@ -1,46 +1,53 @@
 package ru.stqa.pft.adressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.ContactDate;
 import ru.stqa.pft.adressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+	@DataProvider
+	public Iterator<Object[]> validContactFromXml() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+		String xml = "";
+		String line = reader.readLine();
+		while (line != null) {
+			xml += line;
+			line = reader.readLine();
+		}
+		XStream xstream = new XStream();
+		xstream.processAnnotations(ContactDate.class);
+		List<ContactDate> contacts = (List<ContactDate>) xstream.fromXML(xml);
+		return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+	}
 
-	@Test
-	public void testContactCreation() throws Exception {
+	@Test(dataProvider = "validContactFromXml")
+	public void testContactCreation(ContactDate contact) throws Exception {
 		app.goTo().homePage();
 		Contacts before = app.contact().all();
-		File photo = new File("src/test/resources/img.png");
-		ContactDate contact = new ContactDate()
-						.withFirstname("firstname1")
-						.withLastname("lastname1")
-						.withPhoto(photo)
-						.withAddress("Бульвар Олимпийский")
-						.withHomePhone("homephone")
-						.withMobilePhone("mobilephone")
-						.withWorkPhone("workphone")
-						.withEmail("e-mail1")
-						.withEmail2("e-mail12")
-						.withEmail3("e-mail13");
 		app.contact().create(contact, true);
-		// Сравнение размеров множеств
 		Assert.assertEquals(app.contact().count(), before.size() + 1);
 		Contacts after = app.contact().all();
-		// Добавление максимального ID для контакта
-		//contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-
 		int maxId = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
 		contact.withId(maxId);
 		assertThat(after, equalTo(before.withAdded(contact)));
 	}
+
 	@Test(enabled = false)
-	public void testCurrentDir(){
+	public void testCurrentDir() {
 		File currentDir = new File(".");
 		System.out.println(currentDir.getAbsolutePath());
 		File photo = new File("src/test/resources/img.jpg");
