824c7969021f8f19e3fb69b527fc1843d9bf907d
kvlazarenko <kv.lazarenko@gmail.com>
2025-04-08 08:14:16 +0300

    Лекция 7.5. Отключаемые проверки UI.mp4

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -1,17 +1,23 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.adressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.adressbook.model.GroupDate;
+import ru.stqa.pft.adressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class TestBase {
 	Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -40,5 +46,15 @@ public class TestBase {
 
 	public ApplicationManager getApp() {
 		return app;
+	}
+
+	public void verifyGroupListInUI() {
+		if (Boolean.getBoolean("verifyUI")) {
+			Groups dbGroups = app.db().groups();
+			Groups uiGroups = app.group().all();
+			MatcherAssert.assertThat(uiGroups, CoreMatchers.equalTo(dbGroups.stream()
+							.map((g) -> new GroupDate().withId(g.getId()).withName(g.getName()))
+							.collect(Collectors.toSet())));
+		}
 	}
 }
